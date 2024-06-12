8af901d936cd16822f72a131666480e256521af7
calopezg <calopezg@gmail.com>
2024-06-12 17:46:32 -0500

    segundo commit

---
+++ ejercicio1/README.md
@@ -0,0 +1,2 @@
+#Enunciado
+Este ejercicio consiste en codificar una función llamada get_ValAbs que recibe un número entero y retorna un entero con su valor absoluto.
--- ejercicio1/src/main/java/logica_20242/Main.java
+++ ejercicio1/src/main/java/logica_20242/Main.java
@@ -2,10 +2,10 @@ package logica_20242;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("El valor absoluto es: " + get_ValAbs(0));
     }
 
-    public static int get_Absoluto(int nro){
+    public static int get_ValAbs(int nro){
         //Su codigo acá
 
         return 0;
