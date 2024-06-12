8d497fecc71dc58472a3a65705ae2e5558b99fcc
calopezg <calopezg@gmail.com>
2024-06-12 17:42:27 -0500

    Commit inicial

---
+++ ejercicio1/src/main/java/logica_20242/Main.java
@@ -0,0 +1,13 @@
+package logica_20242;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static int get_Absoluto(int nro){
+        //Su codigo acá
+
+        return 0;
+    }
+}
