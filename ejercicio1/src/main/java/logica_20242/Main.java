7b4c50095689f95204512921cf0d36cef22184d7
calopezg <calopezg@gmail.com>
2024-06-13 14:55:57 -0500

    Commit cambio invocacion

--- ejercicio1/src/main/java/logica_20242/Main.java
+++ ejercicio1/src/main/java/logica_20242/Main.java
@@ -2,7 +2,7 @@ package logica_20242;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("El valor absoluto es: " + get_ValAbs(0));
+        System.out.println("El valor absoluto es: \n" + get_ValAbs(-5));
     }
 
     public static int get_ValAbs(int nro){
